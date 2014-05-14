feab592e60d72d52d2eedbea89121a55312e1205
Sarah R Edris <sedris@mit.edu>
2014-05-14 14:44:08 -0400

    can erase from pointsToDraw to update shadow in room

--- Voxelizer/source/interface.cpp
+++ Voxelizer/source/interface.cpp
@@ -314,7 +314,15 @@ void mouseDrawCB(int button, int state, int x, int y)
                     if(i%stride == 0){
                         currShadowPixels.insert(converted);
                     }
-                    pointsToDraw.erase(converted);
+                    std::set<Vector3f>::iterator it;
+                    for (it = pointsToDraw.begin(); it != pointsToDraw.end();){
+                      if (*it == converted){
+                        pointsToDraw.erase(it++);
+                        break;
+                      } else {
+                        ++it;
+                      }
+                    }
                 }
               }
             }
@@ -369,7 +377,15 @@ void mouseDrawMotionCB(int x, int y)
                 if(i%stride == 0){
                     currShadowPixels.insert(converted);
                 }
-                pointsToDraw.erase(converted);
+                std::set<Vector3f>::iterator it;
+                for (it = pointsToDraw.begin(); it != pointsToDraw.end();){
+                  if (*it == converted){
+                    pointsToDraw.erase(it++);
+                    break;
+                  } else {
+                    ++it;
+                  }
+                }
             }
           }
         }
